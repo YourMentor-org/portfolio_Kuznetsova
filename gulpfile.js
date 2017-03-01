d3d94072a2592f4915c1c47dc5c39bb0b1087cbf
annaVyacheslavovna <anya.kuznetsova19@gmail.com>
2017-03-01 14:07:29 +0700

    add linters through Postcss

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require ('gulp-rename');
 var postcss = require('gulp-postcss');
-var reporter = require('postcss-reporter');
+var reporter = require('postcss-browser-reporter');
 var stylelint = require('stylelint');
 var assets = require ('postcss-assets');
 var short = require('postcss-short');
 var handlebars = require('gulp-compile-handlebars');
 var templateContext = require('./src/example.json');
+var config = require('./.stylelintrc.json')
 
 gulp.task('default', ['dev']);
 gulp.task('dev', ['build-dev', 'browser-sync', 'watch']);
@@ -19,16 +20,21 @@ gulp.task('prod', ['clean'], function() {
 		gulp.run('build-dev');
 });
 
-gulp.task('build-dev', ['css-dev', 'assets', 'scripts', 'handlebars', 'css-lint']);
+gulp.task('build-dev', ['css-dev', 'assets', 'scripts', 'handlebars']);
 gulp.task('build-prod', ['css-prod', 'assets', 'scripts', 'handlebars']);
 	
 gulp.task('css-dev', function () {
 	var processors = [
-		short,
+		stylelint(config),
+
 		assets ({
 			loadPaths: ['src/assets/img/'],
 			relativTo: 'src/styles/'
+		}),		
+		reporter({
+			'selector': 'body:before'
 		}),
+		short
   ];
 	return gulp.src('./src/styles/*.css')
 		.pipe(concat('styles.css'))
@@ -104,26 +110,4 @@ gulp.task('handlebars', function (){
 			.pipe(handlebars(templateContext, options))
 			.pipe(rename('index.html'))
 			.pipe(gulp.dest('./build/'));
-});
-
-gulp.task('css-lint', function(){
-	var stylelintConfig = {
-		"rules": {
-			"string-quotes": "double",
-			"function-url-quotes": "always",
-			"declaration-colon-space-after": "never",
-			"indentation": 2
-		}
-	}
-
-	var processors = [
-		stylelint(),
-		/*reporter({
-		"selector": "body:before",
-		}),*/
-	];
-
-	return gulp.src('.src/styles/*.css')
-		.pipe(postcss(processors))
-		.pipe(gulp.dest('./build/styles/'));
 });
